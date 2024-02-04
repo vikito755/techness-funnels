7121a4ff11864aed36d9989567c136fd5dc7c418
Viktor Lazarov <vikitor755@gmail.com>
2024-02-04 16:13:16 +0200

    Change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
+      <title>Techness</title>
     </head>
     <body>
       
