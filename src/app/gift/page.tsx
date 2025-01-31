78ea2e7683c98a2d05c9f7360e14a1b15108a179
Viktor Lazarov <vikitor755@gmail.com>
2025-01-31 14:31:42 +0200

    Add padding.

--- src/app/gift/page.tsx
+++ src/app/gift/page.tsx
@@ -4,7 +4,7 @@ const Thanks = dynamic(() => import("../_components/Thanks"), { ssr: false });
 
 export default function EmailCapture() {
   return (
-    <main className="flex flex-col space-y-5 w-screen h-screen justify-center items-center">
+    <main className="flex flex-col p-4 space-y-5 w-screen h-screen justify-center items-center">
       <Thanks></Thanks>
     </main>
   );
